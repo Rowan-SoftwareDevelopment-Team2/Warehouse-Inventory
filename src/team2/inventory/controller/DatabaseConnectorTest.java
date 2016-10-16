76abf264610bc98fe8017728b44e208761a98e01
James A. Donnell Jr <bubbajim715@verizon.net>
2016-10-16 19:24:41 -0400

    Moved error catching to main() as it is a test controller

--- src/team2/inventory/controller/DatabaseConnectorTest.java
+++ src/team2/inventory/controller/DatabaseConnectorTest.java
@@ -35,191 +35,160 @@ public class DatabaseConnectorTest {
 	/** Testing all tables in database.
 	 * @param args Not utilized. */
 	public static void main(String[] args) {
-		System.out.println("All Barcodes:\n" + getBarcodesTest() + "\n");
-		System.out.println("All Companies:\n" + getCompaniesTest() + "\n");
-		System.out.println("All Items:\n" + getItemsTest() + "\n");
-		System.out.println("All Item Types:\n" + getItemTypesTest() + "\n");
-		System.out.println("All Locations:\n" + getLocationsTest() + "\n");
-		System.out.println("All Privileges:\n" + getPrivilegeTest() + "\n");
-		System.out.println("All Users:\n" + getUsersTest() + "\n");
+		try {
+			System.out.println("All Barcodes:\n" + getBarcodesTest() + "\n");
+			System.out.println("All Companies:\n" + getCompaniesTest() + "\n");
+			System.out.println("All Items:\n" + getItemsTest() + "\n");
+			System.out.println("All Item Types:\n" + getItemTypesTest() + "\n");
+			System.out.println("All Locations:\n" + getLocationsTest() + "\n");
+			System.out.println("All Privileges:\n" + getPrivilegeTest() + "\n");
+			System.out.println("All Users:\n" + getUsersTest() + "\n");
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
 	}
 
 	/** Retrieves all barcodes on database in a map of ID-Barcode.
 	 * @return Map */
-	public static Map<Integer, Integer> getBarcodesTest() {
+	public static Map<Integer, Integer> getBarcodesTest() throws SQLException {
 		Map<Integer, Integer> result = new HashMap<Integer, Integer>();
-		
-		try {
-			Connection connection = DriverManager.getConnection(connectionString);
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("SELECT * FROM `Barcode`");
-			
-			while(stmt.getResultSet().next()) {
-				ResultSet resultSet = stmt.getResultSet();
-				result.put(resultSet.getInt(1), resultSet.getInt(2));
-			}
-			
-			stmt.close();
-			connection.close();
+
+		Connection connection = DriverManager.getConnection(connectionString);
+		Statement stmt = connection.createStatement();
+		stmt.executeUpdate("SELECT * FROM `Barcode`");
+
+		while(stmt.getResultSet().next()) {
+			ResultSet resultSet = stmt.getResultSet();
+			result.put(resultSet.getInt(1), resultSet.getInt(2));
 		}
-		catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		
+
+		stmt.close();
+		connection.close();
+
 		return result;
 	}
-	
+
 	/** Retrieves all companies on database in a map of ID-Company.
 	 * @return Map */
-	public static Map<Integer, Company> getCompaniesTest() {
+	public static Map<Integer, Company> getCompaniesTest() throws SQLException {
 		Map<Integer, Company> result = new HashMap<Integer, Company>();
-		
-		try {
-			Connection connection = DriverManager.getConnection(connectionString);
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("SELECT * FROM `Company`");
-			
-			while(stmt.getResultSet().next()) {
-				ResultSet resultSet = stmt.getResultSet();
-				Company company = new Company(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5));
-				result.put(resultSet.getInt(1), company);
-			}
-			
-			stmt.close();
-			connection.close();
+
+		Connection connection = DriverManager.getConnection(connectionString);
+		Statement stmt = connection.createStatement();
+		stmt.executeUpdate("SELECT * FROM `Company`");
+
+		while(stmt.getResultSet().next()) {
+			ResultSet resultSet = stmt.getResultSet();
+			Company company = new Company(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5));
+			result.put(resultSet.getInt(1), company);
 		}
-		catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		
+
+		stmt.close();
+		connection.close();
+
 		return result;
 	}
-	
+
 	/** Retrieves all items on database in a map of ID-Item.
 	 * @return Map */
-	public static Map<Integer, Item> getItemsTest() {
+	public static Map<Integer, Item> getItemsTest() throws SQLException {
 		Map<Integer, Item> result = new HashMap<Integer, Item>();
-		
-		try {
-			Connection connection = DriverManager.getConnection(connectionString);
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("SELECT * FROM `Item`");
-			
-			while(stmt.getResultSet().next()) {
-				ResultSet resultSet = stmt.getResultSet();
-				Item item = new Item(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3), resultSet.getInt(4), resultSet.getString(5));
-				result.put(resultSet.getInt(1), item);
-			}
-			
-			stmt.close();
-			connection.close();
+
+		Connection connection = DriverManager.getConnection(connectionString);
+		Statement stmt = connection.createStatement();
+		stmt.executeUpdate("SELECT * FROM `Item`");
+
+		while(stmt.getResultSet().next()) {
+			ResultSet resultSet = stmt.getResultSet();
+			Item item = new Item(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3), resultSet.getInt(4), resultSet.getString(5));
+			result.put(resultSet.getInt(1), item);
 		}
-		catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		
+
+		stmt.close();
+		connection.close();
+
 		return result;
 	}
-	
+
 	/** Retrieves all item types on database in a map of ID-ItemType.
 	 * @return Map */
-	public static Map<Integer, String> getItemTypesTest() {
+	public static Map<Integer, String> getItemTypesTest() throws SQLException {
 		Map<Integer, String> result = new HashMap<Integer, String>();
-		
-		try {
-			Connection connection = DriverManager.getConnection(connectionString);
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("SELECT * FROM `ItemType`");
-			
-			while(stmt.getResultSet().next()) {
-				ResultSet resultSet = stmt.getResultSet();
-				result.put(resultSet.getInt(1), resultSet.getString(2));
-			}
-			
-			stmt.close();
-			connection.close();
+
+		Connection connection = DriverManager.getConnection(connectionString);
+		Statement stmt = connection.createStatement();
+		stmt.executeUpdate("SELECT * FROM `ItemType`");
+
+		while(stmt.getResultSet().next()) {
+			ResultSet resultSet = stmt.getResultSet();
+			result.put(resultSet.getInt(1), resultSet.getString(2));
 		}
-		catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		
+
+		stmt.close();
+		connection.close();
+
 		return result;
 	}
-	
+
 	/** Retrieves all locations on database in a map of ID-Location.
 	 * @return Map */
-	public static Map<Integer, Location> getLocationsTest() {
+	public static Map<Integer, Location> getLocationsTest() throws SQLException {
 		Map<Integer, Location> result = new HashMap<Integer, Location>();
-		
-		try {
-			Connection connection = DriverManager.getConnection(connectionString);
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("SELECT * FROM `Location`");
-			
-			while(stmt.getResultSet().next()) {
-				ResultSet resultSet = stmt.getResultSet();
-				Location location = new Location(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3), resultSet.getInt(4));
-				result.put(resultSet.getInt(1), location);
-			}
-			
-			stmt.close();
-			connection.close();
+
+		Connection connection = DriverManager.getConnection(connectionString);
+		Statement stmt = connection.createStatement();
+		stmt.executeUpdate("SELECT * FROM `Location`");
+
+		while(stmt.getResultSet().next()) {
+			ResultSet resultSet = stmt.getResultSet();
+			Location location = new Location(resultSet.getInt(1), resultSet.getString(2), resultSet.getInt(3), resultSet.getInt(4));
+			result.put(resultSet.getInt(1), location);
 		}
-		catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		
+
+		stmt.close();
+		connection.close();
+
 		return result;
 	}
-	
+
 	/** Retrieves all privileges on database in a map of ID-Privilege.
 	 * @return Map */
-	public static Map<Integer, String> getPrivilegeTest() {
+	public static Map<Integer, String> getPrivilegeTest() throws SQLException {
 		Map<Integer, String> result = new HashMap<Integer, String>();
-		
-		try {
-			Connection connection = DriverManager.getConnection(connectionString);
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("SELECT * FROM `Privilege`");
-			
-			while(stmt.getResultSet().next()) {
-				ResultSet resultSet = stmt.getResultSet();
-				result.put(resultSet.getInt(1), resultSet.getString(2));
-			}
-			
-			stmt.close();
-			connection.close();
+
+		Connection connection = DriverManager.getConnection(connectionString);
+		Statement stmt = connection.createStatement();
+		stmt.executeUpdate("SELECT * FROM `Privilege`");
+
+		while(stmt.getResultSet().next()) {
+			ResultSet resultSet = stmt.getResultSet();
+			result.put(resultSet.getInt(1), resultSet.getString(2));
 		}
-		catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		
+
+		stmt.close();
+		connection.close();
+
 		return result;
 	}
-	
+
 	/** Retrieves all users on database in a map of ID-User.
 	 * @return Map */
-	public static Map<Integer, User> getUsersTest() {
+	public static Map<Integer, User> getUsersTest() throws SQLException {
 		Map<Integer, User> result = new HashMap<Integer, User>();
-		
-		try {
-			Connection connection = DriverManager.getConnection(connectionString);
-			Statement stmt = connection.createStatement();
-			stmt.executeUpdate("SELECT * FROM `User`");
-			
-			while(stmt.getResultSet().next()) {
-				ResultSet resultSet = stmt.getResultSet();
-				User user = new User(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getInt(5));
-				result.put(resultSet.getInt(1), user);
-			}
-			
-			stmt.close();
-			connection.close();
+
+		Connection connection = DriverManager.getConnection(connectionString);
+		Statement stmt = connection.createStatement();
+		stmt.executeUpdate("SELECT * FROM `User`");
+
+		while(stmt.getResultSet().next()) {
+			ResultSet resultSet = stmt.getResultSet();
+			User user = new User(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getInt(5));
+			result.put(resultSet.getInt(1), user);
 		}
-		catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		
+
+		stmt.close();
+		connection.close();
+
 		return result;
 	}
 }
